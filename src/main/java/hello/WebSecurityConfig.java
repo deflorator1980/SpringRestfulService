a7d90a5604974a9e4ddd516c82061285d3485717
deflorator1980 <deflorator1980@yandex.ru>
2015-10-08 17:19:18 +0300

    only restful

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .and()
+                .csrf().disable()
             .logout()
                 .permitAll();
     }
