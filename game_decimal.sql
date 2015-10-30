eafc72e8ded914d4353731b4c7c5dbba8882c0f2
deflorator1980 <deflorator1980@yandex.ru>
2015-10-31 01:15:58 +0300

    game_bigdecimal

---
+++ game_decimal.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE game_decimal;
+USE  game_decimal;
+
+CREATE TABLE sales (
+  sale_id  INT         NOT NULL AUTO_INCREMENT,
+  gnome_id VARCHAR(45) NOT NULL,
+  item_id  VARCHAR(45) NOT NULL,
+  quantity INT     NOT NULL,
+  PRIMARY KEY (sale_id)
+);
+
+CREATE TABLE gnomes (
+  gnome_id    VARCHAR(45) NOT NULL,
+  gnome_name  VARCHAR(45) NOT NULL,
+  gnome_money DECIMAL NOT NULL,
+  PRIMARY KEY (gnome_id)
+);
+
+CREATE TABLE items (
+  item_id    VARCHAR(45) NOT NULL,
+  item_name  VARCHAR(45) NOT NULL,
+  item_price DECIMAL     NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+ALTER TABLE sales ADD CONSTRAINT fk_sales_gnomes FOREIGN KEY (gnome_id) REFERENCES gnomes (gnome_id);
+
+ALTER TABLE sales ADD CONSTRAINT fk_sales_items FOREIGN KEY (item_id) REFERENCES items (item_id);
+
+INSERT INTO gnomes (gnome_id, gnome_name, gnome_money) VALUES ('001', 'vova', '10');
+INSERT INTO gnomes (gnome_id, gnome_name, gnome_money) VALUES ('002', 'dasha', '1');
+INSERT INTO gnomes (gnome_id, gnome_name, gnome_money) VALUES ('003', 'yasha', '100');
+
+INSERT INTO items (item_id, item_name, item_price) VALUES ('01', 'sword', 10);
+INSERT INTO items (item_id, item_name, item_price) VALUES ('02', 'spear', 6);
+INSERT INTO items (item_id, item_name, item_price) VALUES ('03', 'grenade', 2);
