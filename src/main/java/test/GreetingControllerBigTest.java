b2fd80fedf8aa8b94040eaf78d689b075ccc188a
deflorator1980 <deflorator1980@yandex.ru>
2015-11-05 22:24:42 +0300

    test view shop

--- src/main/java/test/GreetingControllerBigTest.java
+++ src/main/java/test/GreetingControllerBigTest.java
@@ -1,6 +1,11 @@
 package test;
 
 import hello_big.GreetingControllerBig;
+import hello_big.ShopBig;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +16,32 @@ public class GreetingControllerBigTest {
     @org.junit.Test
     public void testViewShop() throws Exception {
         assertEquals(greetingControllerBig.viewShop(), greetingControllerBig.getItemsList());
+
+        List<ShopBig> listEtalon = new ArrayList<>();
+
+        ShopBig shopBigEtalon = new ShopBig();
+        shopBigEtalon.setId("01");
+        shopBigEtalon.setName("sword");
+        shopBigEtalon.setPrice(new BigDecimal("10"));
+
+        listEtalon.add(shopBigEtalon);
+
+        shopBigEtalon = new ShopBig();
+
+        shopBigEtalon.setId("02");
+        shopBigEtalon.setName("spear");
+        shopBigEtalon.setPrice(new BigDecimal("4"));
+
+        listEtalon.add(shopBigEtalon);
+
+        shopBigEtalon = new ShopBig();
+
+        shopBigEtalon.setId("03");
+        shopBigEtalon.setName("grenade");
+        shopBigEtalon.setPrice(new BigDecimal("2"));
+
+        listEtalon.add(shopBigEtalon);
+
+        assertEquals(listEtalon, greetingControllerBig.getItemsList());
     }
 }
